d79a77741d65f1ea2b18149c3dc172c9ea1bdccb
Ferin <Ferin>
2016-10-10 22:52:33 +0200

    .

--- webpages/CreateAdvert.php
+++ webpages/CreateAdvert.php
@@ -3,6 +3,8 @@
 
 <body>
 	<?php
+	
+	// this needs to change so that when a user selects a categpry we show input fields relative to that
 		include_once dirname(__FILE__) . "/../php/src/schedule/CreateCalendar.php";
 	
 		$arr = array_fill(0, 84, 0);
@@ -13,7 +15,16 @@
 	
 		
 	<form name="myForm" action="CreateAdvert.php" method="POST">
-    <p>Category: <input type='text' name='category' value=""/> </p>
+    <p>Please select a Category: 
+	<select name="categoryType">
+		<option value="education">Education</option>
+		<option value="entertainment">Entertainment</option>
+		<option value="business">Business</option>
+		<option value="other">Other</option>
+	</select> </p>
+	<p>Price per hour :R <input type='number' name='pricePerHour' value=''/></p>
+	<p>Type of service offered: ( ie Math tutor, DJ) <input type='text' name='typeOfService' value=''/> </p>
+	<p>Short description <input type='text' name='description' value=''/> </p>
     <p><input type='submit' name='submit' value='submit'/>
 </form>
 	
